3ac1c57eb7b17122895c250793e2ae5cb018b295
Vladimir Baumgartner <vladimir.baumgartner@devoteam.com>
2018-11-21 09:16:08 +0100

    Removed rollout pause for memory limits

--- Infrastructure/bin/setup_jenkins.sh
+++ Infrastructure/bin/setup_jenkins.sh
@@ -31,9 +31,9 @@ echo "Setting up Jenkins in project ${GUID}-jenkins from Git Repo ${REPO} for Cl
 oc project $GUID-jenkins
 
 oc new-app jenkins-persistent --param ENABLE_OAUTH=true --param MEMORY_LIMIT=4Gi --param VOLUME_CAPACITY=8Gi -n ${GUID}-jenkins
-oc rollout pause dc jenkins -n ${GUID}-jenkins
-oc set resources dc jenkins --limits=memory=4Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n ${GUID}-jenkins
-oc rollout resume dc jenkins -n ${GUID}-jenkins
+#oc rollout pause dc jenkins -n ${GUID}-jenkins
+#oc set resources dc jenkins --limits=memory=4Gi,cpu=2 --requests=memory=2Gi,cpu=1 -n ${GUID}-jenkins
+#oc rollout resume dc jenkins -n ${GUID}-jenkins
 
 # oc process -f Infrastructure/templates/template-jenkins.yml -n ${GUID}-jenkins -p GUID=${GUID} | oc create -n ${GUID}-jenkins -f -
 
